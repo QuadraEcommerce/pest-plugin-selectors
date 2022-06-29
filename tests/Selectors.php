7cc65e988644a3e41d786cff798a7b3856b79034
Scott Carpenter <scott@payforstay.com>
2022-06-29 17:10:27 -0600

    Add "equals" assertions, view layout, and supporting tests
    Added assertions:
    - `assertSelectorEquals(string $selector, string $value)`
    - `assertSelectorNotEquals(string $selector, string $value)`
    - `assertSelectorsAllEqual(string $selector, string $value)`
    - `assertSelectorsAllNotEqual(string $selector, string $value)`
    - `assertSelectorAttributeNotEquals(string $selector, string $attribute, $expected)`

--- tests/Selectors.php
+++ tests/Selectors.php
@@ -10,17 +10,36 @@ it('gets elements matching a selector', function () {
         ->toHaveCount(5);
 });
 
-it('asserts that a selector contains a value', function () {
-    $this->get('/')
-         ->assertSelectorContains('main div.foo .bar', 'baz');
-});
+it("asserts that any of a selector's matches contains a value")
+    ->get('/')
+    ->assertSelectorContains('main div.single .item', 'a value');
 
-it('asserts that multiple selectors all contain the same value', function () {
-    $this->get('/')
-         ->assertSelectorsAllContain('main > ul > li', 'List item');
-});
+it("asserts that all of a selector's matches contain a value")
+    ->get('/')
+    ->assertSelectorsAllContain('main > ul > li', 'List item');
 
-it("asserts that a selector's attribute equals a value", function () {
-    $this->get('/')
-         ->assertSelectorAttributeEquals('footer', 'data-foo', 'bar');
-});
+it("asserts that any of a selector's matches equals a value")
+    ->get('/')
+    ->assertSelectorEquals('main div.single .item', 'a value');
+
+it("asserts that all of a selector's matches equal the same value")
+    ->get('/')
+    ->assertSelectorsAllEqual('main div.multiple-same .item', 'same value');
+
+it("asserts that any of a selector's matches does not equal a value")
+    ->get('/')
+    ->assertSelectorNotEquals('main div.multiple-different .item', 'not the value')
+    ->assertSelectorNotEquals('main div.multiple-different .item', 'same value')
+    ->assertSelectorNotEquals('main div.multiple-different .item', 'different value');
+
+it("asserts that all of a selector's matches do not equal a value")
+    ->get('/')
+    ->assertSelectorsAllNotEqual('main div.multiple-same .item', 'not the value');
+
+it("asserts that a selector match's attribute equals a value")
+    ->get('/')
+    ->assertSelectorAttributeEquals('footer', 'data-foo', 'bar');
+
+it("asserts that a selector match's attribute does not equal a value")
+    ->get('/')
+    ->assertSelectorAttributeNotEquals('footer', 'data-foo', 'not the value');
