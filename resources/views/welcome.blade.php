09d984ca61d60c16219358eb6fbc78d21ef3a21f
Scott Carpenter <scott@goquadra.com>
2023-05-30 22:02:43 -0600

    Update view file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>pest-plugin-selectors test view</title>
+    <title>quadraecom/pest-plugin-selectors test view</title>
 </head>
 <body>
 
@@ -14,7 +15,7 @@
         <li>List item 2</li>
         <li>List item 3</li>
         <li>List item 4</li>
-        <li>List item 5</li>
+        <li data-foo="bar">List item 5</li>
     </ul>
 
     <div class="single">
